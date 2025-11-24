6d2b02ad0621e722bc4fee8f6a47dbe3985b3d8a
luiscastellar <71766787+luiscastellar@users.noreply.github.com>
2025-11-24 16:26:07 +0100

    feat (vfx & map)
    New map
    Vfx added for shooting & hitting
    Animations for shooting & for enemy movement
    Some mechanics polished

--- Assets/_Scripts/Weapon.cs
+++ Assets/_Scripts/Weapon.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] private int damageAumount = 1;
+    [SerializeField] Animator animator;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] ParticleSystem hitVFXPrefab;
+    [SerializeField] int damageAmount = 1;
     
-    StarterAssetsInputs starterAssetsInputs;
+    StarterAssetsInputs _starterAssetsInputs;
 
+    private const string SHOOT_STRING = "Shoot";
+    
     private void Awake()
     {
-        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+        _starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
     }
 
     void Update()
@@ -20,16 +25,21 @@ public class Weapon : MonoBehaviour
 
     void HandleShoot()
     {
-        if (!starterAssetsInputs.shoot) return;
+        if (!_starterAssetsInputs.shoot) return;
+        
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        
+        _starterAssetsInputs.ShootInput(false);
         
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {   
+            Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
+            
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
-            enemyHealth?.TakeDamage(damageAumount);
-                
-            starterAssetsInputs.ShootInput(false);
+            enemyHealth?.TakeDamage(damageAmount);
         }
     }
 }
